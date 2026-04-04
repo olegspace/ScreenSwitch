1a73a8160cf09a9ce2cfba6f38110628cce880bd
olegspace <olezhka_revin@mail.ru>
2026-04-04 12:26:34 +0700

    Upgrade theme for AboutActivity

--- app/src/main/java/com/example/screenswitch/AboutActivity.java
+++ app/src/main/java/com/example/screenswitch/AboutActivity.java
@@ -1,9 +1,11 @@
 package com.example.screenswitch;
 
-import android.app.Activity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-public class AboutActivity  extends Activity {
+public class AboutActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
